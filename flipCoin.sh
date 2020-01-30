6dda8c9074fc8c841748046858e97a9bf7a2c1e1
kn52 <ashish52922@gmail.com>
2020-01-30 12:10:14 +0530

    ADDED:outcome function to display head or tail

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "Welcome to flip coin problem"
+
+outcome()
+{
+	if [ $((RANDOM%2)) == 1 ]
+        then
+               	echo "Head"
+        else
+               	echo "Tail"
+        fi
+}
+outcome
